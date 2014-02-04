56db9b869e0a8dfd81fd4be12e1300d8e10332bc
David Higgins <dave@uiginn.com>
2014-02-04 12:22:31 -0600

    Version used for first test run with new parameter set.

--- GeneralIncludes.h
+++ GeneralIncludes.h
@@ -34,7 +34,7 @@
 #define GAUSSIAN_SYNAPTIC_SEED (-12)
 #define UNIFORM_SYNAPTIC_SEED (-11)
 
-#define MAX_TIME_STEPS (30) /*(12000000)*/ /*(12000000)*/ /*(300000)*/ /*no of timesteps, each of size dt*/
+#define MAX_TIME_STEPS (300000) /*(12000000)*/ /*(12000000)*/ /*(300000)*/ /*no of timesteps, each of size dt*/
 
 // Network schema
 #define NO_EXC (8000) /*(400)*/ /*(10000)*/
@@ -43,11 +43,11 @@
 #define CONNECTIVITY_PROBABILITY (0.05) /*(0.05)*/
 
 // Time step sizes and statistical bin widths
-#define LIF_DT (0.1) /* modify refrac time and calcium delay in tandem, also MAX_TIME_STEPS */
+#define LIF_DT (0.00001) /* modify refrac time and calcium delay in tandem, also MAX_TIME_STEPS */
 #define SYN_DT LIF_DT /*TODO: at a later stage I will have the synapse update more slowly than the lif*/
 #define BIN_SIZE (1.) /*(0.1)*/
 
-#define SYN_DYN_AMPA_DELAY LIF_DT
+#define SYN_DYN_AMPA_DELAY (0.001) /*LIF_DT*/
 
 // Stimulation of subpopulation /* using secs despite inconsistency with other parameter units */
 #define STIM_ON (0.)
@@ -59,10 +59,10 @@
 // Transfer voltages
 #define J_EE (0.2) /*(0.1)*/
 #define J_IE (0.1)
-#define J_II (-0.4)
-#define J_EI (-0.4)
+#define J_II (-0.2) /*(-0.4)*/
+#define J_EI (-0.2) /*(-0.4)*/
 
-#define J_EXT (11.046) /*(15.315)*/ /*(11.046)*/ /*(11.45) new in-vivo*/ /*(11.046) new in-vitro*/ /*(7.07)*/ /*(6.966) in-vivo*/ /*(7.07) 1hz in-vitro*/
+#define J_EXT (15) /*(11.046)*/ /*(15.315)*/ /*(11.046)*/ /*(11.45) new in-vivo*/ /*(11.046) new in-vitro*/ /*(7.07)*/ /*(6.966) in-vivo*/ /*(7.07) 1hz in-vitro*/
 
 
 // LIF specific
@@ -73,28 +73,27 @@
 #define LIF_CM (0.001)
 #define LIF_RM (20.0)
 #define LIF_SIGMA (5)
-#define LIF_REFRAC_TIME_EXC (0) /*200*/ /*timesteps*/
-#define LIF_REFRAC_TIME_INH (0) /*200*/ /*timesteps*/
+#define LIF_REFRAC_TIME_EXC (2000) /*200*/ /*timesteps*/
+#define LIF_REFRAC_TIME_INH (1000) /*200*/ /*timesteps*/
 
 // Synapse plasticity model specific
-#define SYN_RHO_INITIAL (0.164840) /*(0.406595)*/ /*(0.019) in-vivo*/ /*(0.164840) new in-vitro*/ /*(0.16492)*/ /*(0.203586)*/ /*(1.0)*/
+#define SYN_RHO_INITIAL (0.084525) /*(0.164840)*/ /*(0.406595)*/ /*(0.019) in-vivo*/ /*(0.164840) new in-vitro*/ /*(0.16492)*/ /*(0.203586)*/ /*(1.0)*/
 #define SYN_CA_INITIAL (0.0)
 #define SYN_CALCIUM_DELAY (461) /*46*/ /*4.6098ms*/ /*timesteps (needs to be modified when DT is modified above*/
-#define SYN_GAMMA_P (725.085)
-#define SYN_GAMMA_D (331.909)
-#define SYN_THETA_P (1.3)
+#define SYN_GAMMA_P (20000) /*(725.085)*/
+#define SYN_GAMMA_D (500) /*(331.909)*/
+#define SYN_THETA_P (2.) /*(1.3)*/
 #define SYN_THETA_D (1.0)
 #define SYN_SIGMA (3.35) /*(3.35)*/ /*3.35;*/ /*TODO: switch synapse noise back on*/
-#define SYN_TAU (346.3615)
-#define SYN_TAU_CA (0.0226936)
-#define SYN_C_PRE (0.56175) /*(0.33705)*/ /*(0.56175)*/ /*(0.5617539)*/
-#define SYN_C_POST (1.23964) /*(0.74378)*/ /*(1.23964)*/
+#define SYN_TAU (300) /*(346.3615)*/
+#define SYN_TAU_CA (0.020) /*(0.0226936)*/
+#define SYN_C_PRE (0.45) /*(0.56175)*/ /*(0.33705)*/ /*(0.56175)*/ /*(0.5617539)*/
+#define SYN_C_POST (0.45) /*(1.23964)*/ /*(0.74378)*/ /*(1.23964)*/
 
 #define SYN_RHO_FIXED SYN_RHO_INITIAL /*(0.5)*/
 
 // Synaptic current dynamics
 //#define SYN_DYN_AMPA_DELAY (0.001)
-//#define SYN_DYN_AMPA_DELAY (0.00001)
 #define SYN_DYN_NMDA_DELAY SYN_DYN_AMPA_DELAY
 #define SYN_DYN_GABA_DELAY SYN_DYN_AMPA_DELAY
 #define SYN_DYN_TAU_AMPA_RISE (0.0005)
